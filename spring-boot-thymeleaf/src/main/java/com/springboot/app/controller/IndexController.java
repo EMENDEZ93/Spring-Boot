39157415d39d48ded53d072d0426eb406b2c8290
- Edwin Mendez - <emendez@unac.edu.co>
2018-07-14 21:10:37 -0500

    @value global attribute

--- spring-boot-thymeleaf/src/main/java/com/springboot/app/controller/IndexController.java
+++ spring-boot-thymeleaf/src/main/java/com/springboot/app/controller/IndexController.java
@@ -1,5 +1,6 @@
 package com.springboot.app.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,9 +8,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class IndexController {
 
+	@Value("${application.controller.mensaje}")
+	private String mensaje;
+	
 	@RequestMapping("/index")
 	public String index(Model model) {
-		model.addAttribute("mensaje", "- Spring Boot -");
+		model.addAttribute("mensaje", this.mensaje);
 		return "index";
 	}
 	
