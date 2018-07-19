a2883c32429e26e58b14e4c01ce3d638cc982e8d
Edwin Mendez <emendez@gravitalsoftware.com>
2018-07-19 11:38:27 -0500

    valid data

--- jpa/spring-boot-jpa-/src/main/java/com/springboot/app/controller/IndexController.java
+++ jpa/spring-boot-jpa-/src/main/java/com/springboot/app/controller/IndexController.java
@@ -2,9 +2,12 @@ package com.springboot.app.controller;
 
 import java.util.Map;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -40,7 +43,12 @@ public class IndexController {
 	
 	
 	@RequestMapping(value="/form", method=RequestMethod.POST)
-	public String crear( Cliente cliente) {
+	public String crear(@Valid Cliente cliente, BindingResult result) {
+		
+		if(result.hasErrors()) {
+			return "form";
+		}
+				
 		clienteDao.save(cliente);
 		return "redirect:listar";
 	}		
