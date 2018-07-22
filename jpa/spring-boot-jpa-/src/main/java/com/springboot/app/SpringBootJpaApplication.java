23aed5d58aafc288ea03db07397dc1c8ca2708ec
Edwin Mendez <emendez@gravitalsoftware.com>
2018-07-22 18:34:13 -0500

    parameter init - run-restart

--- jpa/spring-boot-jpa-/src/main/java/com/springboot/app/SpringBootJpaApplication.java
+++ jpa/spring-boot-jpa-/src/main/java/com/springboot/app/SpringBootJpaApplication.java
@@ -1,12 +1,25 @@
 package com.springboot.app;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class SpringBootJpaApplication {
+import com.springboot.app.models.service.IUploadFileService;
 
+@SpringBootApplication
+public class SpringBootJpaApplication implements CommandLineRunner {
+
+	@Autowired
+	IUploadFileService uploadFileService;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootJpaApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		uploadFileService.deleteAll();
+		uploadFileService.init();
 	}
 }
