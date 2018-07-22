8bbd54e22ce7716b05d93c9077df9808a6b8bd36
Edwin Mendez <emendez@gravitalsoftware.com>
2018-07-22 11:19:47 -0500

    detail view 'ver'

--- jpa/spring-boot-jpa-/src/main/java/com/springboot/app/controller/IndexController.java
+++ jpa/spring-boot-jpa-/src/main/java/com/springboot/app/controller/IndexController.java
@@ -131,4 +131,32 @@ public class IndexController {
 		return "redirect:/listar";
 	}	
 	
+	
+	@RequestMapping(value="/ver/{id}")
+	public String ver(@PathVariable(value="id")Long id, Map<String, Object> model, RedirectAttributes flash) {
+		Cliente cliente = clienteService.findOne(id);
+		
+		if(cliente == null) {
+			flash.addFlashAttribute("error", "Cliente no existe en la base de datos");
+			return "redirect:/listar"; 
+		}
+		
+		model.put("cliente", cliente);
+		model.put("titulo", "detalle cliente");
+		return "ver";
+		
+	}
+	
+	
 }
+
+
+
+
+
+
+
+
+
+
+
